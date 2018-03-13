7e8eaf4968c95406c695b99fb8b90c69886b8649
Yaoxiang Li <liyaoxiang@outlook.com>
2018-03-13 03:38:54 -0400

    stage2

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Board
 
-# Create your views here.
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
--- bulletin_boards/wsgi.py
+++ bulletin_boards/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bulletin_boards project.
+WSGI config for myproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
